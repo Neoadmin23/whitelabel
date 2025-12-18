aeb15e03c4bdbeff1a71dffd4e175d83b3a0a754
Neotech <123390754+Neoadmin23@users.noreply.github.com>
2025-12-19 02:04:12 +0530

    merge the file

--- whitelabel/__init__.py
+++ whitelabel/__init__.py
@@ -1,10 +1,2 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-import frappe
-
-__version__ = '0.0.1'
-
-if frappe.conf and frappe.conf.get("app_logo_url"):
-    __logo__ = frappe.conf.get("app_logo_url") or '/assets/whitelabel/images/whitelabel_logo.jpg'
-else:
-    __logo__ = '/assets/whitelabel/images/whitelabel_logo.jpg'
+__version__ = "0.0.1"
+# Keep __init__ import-safe; do not import frappe here.
