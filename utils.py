bcba2697bf22b73a92f6630ec2dbe44a71f4c4f8
Neotech <123390754+Neoadmin23@users.noreply.github.com>
2025-12-19 02:03:36 +0530

    merge the file

--- utils.py
+++ utils.py
@@ -1,9 +1,8 @@
-
-# whitelabel/utils.py
 import frappe
 
 DEFAULT_LOGO = "/assets/whitelabel/images/whitelabel_logo.jpg"
 
 def get_logo() -> str:
-    """Return the app logo URL based on frappe.conf, falling back    """Return the app logo URL based on frappe.conf, falling back to default."""
+    """Return the app logo URL based on frappe.conf, falling back to default."""
     url = frappe.conf and frappe.conf.get("app_logo_url")
+    return url or DEFAULT_LOGO
